163d0139ee2bfe13e7d10d5a1d632eaf11f6b6a4
fatimaezzahraadyani <fatimaezzahraadyani@gmail.com>
2025-08-07 11:56:42 +0200

    book Model

--- src/main/java/com/cataloguedelivre/miniproject/Model/Book.java
+++ src/main/java/com/cataloguedelivre/miniproject/Model/Book.java
@@ -1,7 +1,49 @@
 package com.cataloguedelivre.miniproject.Model;
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
 
 @Entity
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String isbn;
+
+    @ManyToOne
+    @JoinColumn(name = "author_id", nullable = false)
+    private Author author;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public Author getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Author author) {
+        this.author = author;
+    }
 }
