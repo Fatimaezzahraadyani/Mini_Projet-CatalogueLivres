bd81d041b6d826b7bae1ee7afdca53fbd068cd36
fatimaezzahraadyani <fatimaezzahraadyani@gmail.com>
2025-08-07 12:07:37 +0200

    author model

--- src/main/java/com/cataloguedelivre/miniproject/Model/Author.java
+++ src/main/java/com/cataloguedelivre/miniproject/Model/Author.java
@@ -1,9 +1,52 @@
 package com.cataloguedelivre.miniproject.Model;
 
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+
+import java.util.List;
 
 @Entity
 public class Author {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String biography;
+
+    @OneToMany(mappedBy = "author" , cascade = CascadeType.ALL)
+    private List<Book> books;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBiography() {
+        return biography;
+    }
+
+    public void setBiography(String biography) {
+        this.biography = biography;
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(List<Book> books) {
+        this.books = books;
+    }
 }
